43ddd2f54c1f90646d3fc1475b6b9323c7fd165f
wilczkowo <44966142+wilczkowo@users.noreply.github.com>
2021-02-25 14:33:52 -0500

    praca czwartek

--- todo/todo.js
+++ todo/todo.js
@@ -1,33 +1,39 @@
 // selectors
-const formInput = document.querySelector('.todo-input')
-const formButton = document.querySelector('.todo-button')
-const listContainer = document.querySelector('.todo-container')
-const list = document.querySelector('.todo-list')
+const formInput = document.querySelector(".todo-input");
+const formButton = document.querySelector(".todo-button");
+const listContainer = document.querySelector(".todo-container");
+const list = document.querySelector(".todo-list");
 
 // events
 
-formButton.addEventListener('click', addTodo)
+formButton.addEventListener("click", addTodo);
 
 // functions
 
 function addTodo(e) {
-  e.preventDefault()
-  console.log('ala')
+  e.preventDefault();
+  console.log("ala");
 }
 
-function MyHereDoc(){
+function regularExpresion() {
+  let mojString = "bonib@onic.om";
+  let regEx1 = /.*@.*\..*/;
+  if(regEx1.test(mojString)){
+    console.log('poprawny email');
+  }
 
-        let mojString = 'ala ma kota'    
-        var reobj = new RegExp("/\\*"+here+"\\n[\\s\\S]*?\\n"+here+"\\*/", "m");
-        str = reobj.exec(MyHereDoc).toString();
-        str = str.replace(new RegExp("/\\*"+here+"\\n",'m'),'').toString();
-        return str.replace(new RegExp("\\n"+here+"\\*/",'m'),'').toString();
-    }
-    
 
-{/* <div class="todo">
+
+}
+
+
+regularExpresion();
+
+
+
+/* <div class="todo">
   <li>
     <button></button>
     <button></button>
   </li>
-</div> */}
+</div> */
