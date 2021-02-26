9b8dd82a7b8d827c4fb4e2aa3564c1194ade0cdb
“wilczkowo” <44966142+wilczkowo@users.noreply.github.com>
2021-02-26 05:07:29 -0500

    dom piatek

--- todo/todo.js
+++ todo/todo.js
@@ -12,32 +12,40 @@ formButton.addEventListener("click", addTodo);
 // functions
 
 function addTodo(e) {
-  e.preventDefault();
-  const liWrapper = document.createElement('div');
-  liWrapper.classList.add('liWrapper');
-  
-  const todoLi = document.createElement('li');
-  todoLi.innerText = 'tekst';
-  todoLi.classList.add('todoLi');
-  liWrapper.appendChild(todoLi);
 
-  const todoCompleteButton = document.createElement('button');
-  todoCompleteButton.innerHTML = '<i class="fas fa-check"></i>';
-  todoCompleteButton.classList.add('todoCompleteButton');
-  todoLi.appendChild(todoCompleteButton);
+    // create div
+    e.preventDefault();
+    const liWrapper = document.createElement('div');
+    liWrapper.classList.add('liWrapper');
 
-  const todoTrashButton = document.createElement('button');
-  todoTrashButton.innerHTML = '<i class="fas fa-trash"></i>';
-  todoTrashButton.classList.add('todoTrashButton');
-  todoLi.appendChild(todoTrashButton);
+    // add li to div
+    const todoItem = document.createElement('li');
+    todoItem.innerText = formInput.value;
+    todoItem.classList.add('todoItem');
+    liWrapper.appendChild(todoItem);
 
-  list.appendChild(liWrapper);
+    // add check to div
+    const todoCompleteButton = document.createElement('button');
+    todoCompleteButton.innerHTML = '<i class="fas fa-check"></i>';
+    todoCompleteButton.classList.add('todoCompleteButton');
+    liWrapper.appendChild(todoCompleteButton);
+    todoCompleteButton.addEventListener('click', toggleClassCompleteForItem);
+
+    // add trash button to div
+    const todoTrashButton = document.createElement('button');
+    todoTrashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    todoTrashButton.classList.add('todoTrashButton');
+    liWrapper.appendChild(todoTrashButton);
+    todoTrashButton.addEventListener('click', removeTodoItem);
+
+
+    //add div to ul
+    list.appendChild(liWrapper);
+    // clear input on form
+    formInput.value = "";
 
 }
-const ala = getComputedStyle(root).getPropertyValue('--dark');
-root.style.setProperty('--gradientA','brown');
-console.log(ala);
 
-
-
+const removeTodoItem = (e) => e.target.parentElement.remove();
+const  toggleClassCompleteForItem = (e)=> e.target.parentElement.classList.toggle('complete');
 
