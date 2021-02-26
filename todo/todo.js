203d7e7ea8793771aa78a62194f702f77c3f6dcf
wilczkowo <44966142+wilczkowo@users.noreply.github.com>
2021-02-26 13:26:58 -0500

    praca piatek

--- todo/todo.js
+++ todo/todo.js
@@ -12,40 +12,41 @@ formButton.addEventListener("click", addTodo);
 // functions
 
 function addTodo(e) {
+  // create div
+  e.preventDefault();
+  const liWrapper = document.createElement("div");
+  liWrapper.classList.add("liWrapper");
 
-    // create div
-    e.preventDefault();
-    const liWrapper = document.createElement('div');
-    liWrapper.classList.add('liWrapper');
+  // add li to div
+  const todoItem = document.createElement("li");
+  todoItem.innerText = formInput.value;
+  todoItem.classList.add("todoItem");
+  liWrapper.appendChild(todoItem);
 
-    // add li to div
-    const todoItem = document.createElement('li');
-    todoItem.innerText = formInput.value;
-    todoItem.classList.add('todoItem');
-    liWrapper.appendChild(todoItem);
+  // add check to div
+  const todoCompleteButton = document.createElement("button");
+  todoCompleteButton.innerHTML = '<i class="fas fa-check"></i>';
+  todoCompleteButton.classList.add("todoCompleteButton");
+  liWrapper.appendChild(todoCompleteButton);
+  todoCompleteButton.addEventListener("click", toggleClassCompleteForItem);
 
-    // add check to div
-    const todoCompleteButton = document.createElement('button');
-    todoCompleteButton.innerHTML = '<i class="fas fa-check"></i>';
-    todoCompleteButton.classList.add('todoCompleteButton');
-    liWrapper.appendChild(todoCompleteButton);
-    todoCompleteButton.addEventListener('click', toggleClassCompleteForItem);
+  // add trash button to div
+  const todoTrashButton = document.createElement("button");
+  todoTrashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  todoTrashButton.classList.add("todoTrashButton");
+  liWrapper.appendChild(todoTrashButton);
+  todoTrashButton.addEventListener("click", removeTodoItem);
 
-    // add trash button to div
-    const todoTrashButton = document.createElement('button');
-    todoTrashButton.innerHTML = '<i class="fas fa-trash"></i>';
-    todoTrashButton.classList.add('todoTrashButton');
-    liWrapper.appendChild(todoTrashButton);
-    todoTrashButton.addEventListener('click', removeTodoItem);
-
-
-    //add div to ul
-    list.appendChild(liWrapper);
-    // clear input on form
-    formInput.value = "";
-
+  //add div to ul
+  list.appendChild(liWrapper);
+  // clear input on form
+  formInput.value = "";
 }
 
-const removeTodoItem = (e) => e.target.parentElement.remove();
-const  toggleClassCompleteForItem = (e)=> e.target.parentElement.classList.toggle('complete');
-
+const removeTodoItem = (e) => {
+  let item = e.target.parentElement;
+  item.classList.add("fall");
+  item.addEventListener('transitionend',()=>item.remove());
+};
+const toggleClassCompleteForItem = (e) =>
+  e.target.parentElement.classList.toggle("complete");
