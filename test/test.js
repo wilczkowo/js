506825e406eefede2195f8e139dc07b3c2f6b7ee
wilczkowodevelop <et.djanus@gmail.com>
2021-03-28 05:08:29 -0400

    koniec js

--- test/test.js
+++ test/test.js
@@ -1,5 +1,32 @@
-const intro = document.querySelector('.intro');
-const video = intro.querySelector('video');
-const text = intro.querySelector('h1');
+function pobierzPorade() {
+	return new Promise((resolve, reject) => {
+		let porada = null;
+		porada = fetch('https://api.adviceslip.com/advice')
+		if (porada) {
+			resolve(porada)
+		} else {
+			reject(new Error('Brak porady z internetu'))
+		}
+	})
+}
+
+function pobierzTempZMicroSoft(){
+	return new Promise((resolve, reject) => {
+		let temperatura = null;
+		temperatura = 20;
+		if (temperatura){resolve(temperatura)}
+		else {reject('nie ma danych temperatury')}
+	})}
+
+async function pobierzDaneZInternetu(){
+	const porada = await pobierzPorade();
+	const poradaJson = await porada.json();
+	const temperatura = await pobierzTempZMicroSoft();
+	console.log(poradaJson.slip)
+	console.log(temperatura);
+
+}
+
+pobierzDaneZInternetu();
 
 
